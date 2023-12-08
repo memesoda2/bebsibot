f86a9332ecdb1ce27ae3e37e0f7173bd7fe44b2d
Minion6011 <mvittorio29@gmail.com>
2023-12-08 17:38:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -81,24 +81,8 @@ async def on_member_join(member):
     role = discord.utils.get(member.guild.roles, id=1034914089807400970)
     await member.add_roles(role)
 
-@client.tree.command(name="reportbug", description="Segnala un bug") #slash command
-async def report_bug(interaction: discord.Interaction):
-  modal = BugModal
-  await interaction.response.send_modal(BugModal())
 
-class BugModal(ui.Modal, title='Report Bug'):
-    bug_name = ui.TextInput(label='Bugged Command name',required=True,placeholder='Bugged command name...', max_length=50)
-    answer = ui.TextInput(label='Description of the bug', style=discord.TextStyle.paragraph, max_length=300,placeholder='Bug description...')
 
-    async def on_submit(self, interaction: discord.Interaction):
-        channel = client.get_channel(1164195802047053894)
-        embed = discord.Embed(title="Bug report ",color=discord.Color.green())
-        embed.add_field(name="Bugged Command name", value=self.children[0].value)
-        embed.add_field(name="Description of the bug", value=self.children[1].value)
-        embed.add_field(name="User:", value=f"`{interaction.user}`")
-        await channel.send(embed=embed)
-        embed1 = discord.Embed(title="Bug report sent", color=discord.Color.green())
-        await interaction.response.send_message(embeds=[embed1], ephemeral=True)
 
 
 
